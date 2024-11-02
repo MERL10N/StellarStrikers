7b469f0b621774a08ecc64f30d5f7c2cd328e086
M3RL10N <marvi_kian_heydari@student.smc.edu>
2024-11-01 23:03:57 -0700

    Added raylib to .gitmodule file

--- Source/main.cpp
+++ Source/main.cpp
@@ -12,14 +12,13 @@ int main()
     while (!WindowShouldClose())
     {
         BeginDrawing();
-
         ClearBackground(RAYWHITE);
 
         const int texture_x = width * 0.5f - texture.width * 0.5f;
         const int texture_y = height * 0.5f - texture.height * 0.5f;
         DrawTexture(texture, texture_x, texture_y, WHITE);
 
-        const char* text = "OMG! IT WORKS!";
+        const char* text = "CS 100 Final Project";
         const Vector2 text_size = MeasureTextEx(GetFontDefault(), text, 20, 1);
         DrawText(text, width / 2 - text_size.x / 2, texture_y + texture.height + text_size.y + 10, 20, BLACK);
 
