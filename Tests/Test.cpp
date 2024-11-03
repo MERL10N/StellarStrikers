62018f26fab8d795e79eb5db6368e0a0e88537eb
M3RL10N <marvi_kian_heydari@student.smc.edu>
2024-11-02 19:09:25 -0700

    Added GoogleTest framework and test folder

---
+++ Tests/Test.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+
+// Sample test
+TEST(SampleTest, BasicAssertions)
+{
+    EXPECT_EQ(1, 1);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
