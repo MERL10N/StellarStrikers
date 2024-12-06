4bf1148aec57bd54520073271998d39ef1282e4b
M3RL10N <marvi_kian_heydari@student.smc.edu>
2024-12-06 00:18:28 -0800

    Added waveManager class

---
+++ Source/waveManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Kian Marvi on 12/6/24.
+//
+
+#include "waveManager.h"
---
+++ Source/waveManager.h
@@ -0,0 +1,16 @@
+//
+// Created by Kian Marvi on 12/6/24.
+//
+
+#ifndef WAVEMANAGER_H
+#define WAVEMANAGER_H
+
+
+
+class waveManager {
+
+};
+
+
+
+#endif //WAVEMANAGER_H
